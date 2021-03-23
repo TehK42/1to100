91a20b5e6a25e5283efd2d42a60f915ffd7b45f2
unknown <tupazluisgabriel@gmail.com>
2021-03-23 11:57:47 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        int num;
+        int total = 0;
+
+        for(num = 1; num <= 100; num++){
+            total = total + num;
+        }
+        System.out.println(total);
+    }
+}
